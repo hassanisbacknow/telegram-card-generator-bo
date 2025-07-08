ce230162d9bd8aa43714f413ad8ba2d66d5fee46
hassanisbacknow <muhammadhassan20154@gmail.com>
2025-07-08 13:26:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def main():
     ping_self()  # background pinger
     print("Bot is running...")
 
-    application = ApplicationBuilder().token("7654475659:AAHDMOaoQ35q72_D5hFc793dwivayMJGxa4").build()
+    application = ApplicationBuilder().token("7654475659:AAGg99VfJrm_4ABGG7WbRctClinsM7Z1tkQ").build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("gen", gen))
